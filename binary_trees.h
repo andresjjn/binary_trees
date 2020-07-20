46f3a013b2f8317af82494fd1589c10f6cfc0b58
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-07-20 17:03:55 +0000

    Add: binary_tree_print.c Update: binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,8 @@
 #ifndef binary_trees_h
 #define binary_trees_h
 
+#include <stddef.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -11,14 +13,16 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *);
 
 #endif
